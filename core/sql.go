3f3a94869a8a545d28db7b764756f39705aaacc9
1377195627 <1377195627@qq.com>
2018-01-25 00:15:57 +0800

    添加sql.go

---
+++ core/sql.go
@@ -0,0 +1,14 @@
+package core
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func GetSqlConn() (*gorm.DB,error) {
+	db := Conf.Db
+	args:=fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=UTC",
+		db.Username,db.Password,db.Address,db.Port,db.Dbname)
+	return gorm.Open("mysql",args)
+}
