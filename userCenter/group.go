1281c0e714310e12d218d55bf255060be7fabb84
xiayesuifeng <1377195627@qq.com>
2018-03-25 23:17:37 +0800

    add user center

---
+++ userCenter/group.go
@@ -0,0 +1,81 @@
+package userCenter
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/firerainos/firerain-web-go/core"
+	"errors"
+)
+
+type Group struct {
+	gorm.Model
+	Name string
+}
+
+func AddGroup(name string) error {
+	db, err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Create(&Group{Name: name}).Error
+}
+
+func GetGroup() ([]Group, error) {
+	db, err := core.GetSqlConn()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var groups []Group
+	db.Find(&groups)
+
+	return groups, nil
+}
+
+func GetGroupByName(name string) (Group, error) {
+	var group Group
+
+	db, err := core.GetSqlConn()
+	if err != nil {
+		return group, err
+	}
+	defer db.Close()
+
+	if db.Where("name = ?", name).First(&group).RecordNotFound() {
+		return group, errors.New("group " + name + " not found")
+	}
+
+	return group, nil
+}
+
+func GetGroupByNames(names []string) ([]Group, error) {
+	var groups []Group
+
+	db, err := core.GetSqlConn()
+	if err != nil {
+		return groups, err
+	}
+	defer db.Close()
+
+	for _, name := range names {
+		group := Group{}
+		if db.Where("name = ?", name).First(&group).RecordNotFound() {
+			return groups, errors.New("group " + name + " not found")
+		}
+		groups = append(groups, group)
+	}
+
+	return groups, nil
+}
+
+func DeleteGroup(name string) error {
+	db, err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Where("name = ?", name).Delete(&Group{}).Error
+}
---
+++ userCenter/user.go
@@ -0,0 +1,123 @@
+package userCenter
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/firerainos/firerain-web-go/core"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Password string
+	Email string
+	Group []Group
+}
+
+func AddUser(username,password,email string,group []string) error {
+	g,err := GetGroupByNames(group)
+	if err != nil {
+		return err
+	}
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Create(&User{Username:username,Password:password,Email:email,Group:g}).Error
+}
+
+func GetUser() ([]User,error) {
+	var users []User
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return users,err
+	}
+	defer db.Close()
+
+	db.Find(&users)
+
+	return users, nil
+}
+
+func GetUserByName(name string) (User,error) {
+	var user User
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return user,err
+	}
+	defer db.Close()
+
+	if db.Where("name = ?",name).First(&user).RecordNotFound() {
+		return user, db.Error
+	}
+
+	return user, nil
+}
+
+func GetUserById(id int) (User,error) {
+	var user User
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return user,err
+	}
+	defer db.Close()
+
+	if db.Where("id = ?",id).First(&user).RecordNotFound() {
+		return user, db.Error
+	}
+
+	return user, nil
+}
+
+func (user User) Delete() error {
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	db.Delete(user)
+
+	return nil
+}
+
+func (user User) AddGroup(group string) error {
+	g,err := GetGroupByName(group)
+	if err != nil {
+		return err
+	}
+
+	user.Group = append(user.Group, g)
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Save(user).Error
+}
+
+func (user User) DeleteGroup(group string) error {
+	var groups []Group
+	for _,g := range user.Group {
+		if g.Name != group {
+			groups = append(groups, g)
+		}
+	}
+
+	user.Group = groups
+
+	db,err := core.GetSqlConn()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Save(user).Error
+}
