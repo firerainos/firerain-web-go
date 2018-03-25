d0e0808337b40f60990cf5ffb305b68c60e337ab
xiayesuifeng <1377195627@qq.com>
2018-03-25 23:44:03 +0800

    done getUser

--- api/userCenter.go
+++ api/userCenter.go
@@ -1,6 +1,9 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/firerainos/firerain-web-go/userCenter"
+)
 
 func AddUser(ctx *gin.Context) {
 
@@ -11,7 +14,19 @@ func DeleteUser(ctx *gin.Context) {
 }
 
 func GetUser(ctx *gin.Context) {
+	users,err := userCenter.GetUser()
+	if err != nil {
+		ctx.JSON(200,gin.H{
+			"code":104,
+			"message":err.Error(),
+		})
+		return
+	}
 
+	ctx.JSON(200,gin.H{
+		"code":0,
+		"users": users,
+	})
 }
 
 
