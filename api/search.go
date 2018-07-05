e00d90b5c25cb2bb7215ae62ecf58c9806c64b37
xiayesuifeng <gdigrgg@gmail.com>
2018-07-05 15:58:10 +0800

    add search api

---
+++ api/search.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/firerainos/firerain-web-go/search"
+)
+
+func Search(ctx *gin.Context) {
+	arch := ctx.Query("arch")
+	repo := ctx.Query("repo")
+	query := ctx.Query("query")
+	maintainer := ctx.Query("maintainer")
+	flagged := ctx.Query("flagged")
+
+	pkgs,num,pages := search.GetPackages(arch,repo,query,maintainer,flagged)
+
+	ctx.JSON(200,gin.H{
+		"code":0,
+		"packages":pkgs,
+		"num":num,
+		"pages":pages,
+	})
+}
+
+func GetArch(ctx *gin.Context) {
+	ctx.JSON(200,gin.H{
+		"code": 0,
+		"arch": search.GetArch(),
+	})
+}
+
+func GetRepo(ctx *gin.Context) {
+	ctx.JSON(200,gin.H{
+		"code": 0,
+		"repo": search.GetRepository(),
+	})
+}
+
+func GetMaintainer(ctx *gin.Context) {
+	ctx.JSON(200,gin.H{
+		"code":       0,
+		"maintainer": search.GetMaintainer(),
+	})
+}
+
+func GetFlagged(ctx *gin.Context) {
+	ctx.JSON(200,gin.H{
+		"code":    0,
+		"flagged": search.GetFlagged(),
+	})
+}
